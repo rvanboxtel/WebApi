12f6657d3c7966affb2200e1ccc46791db92ceb9
rvanboxtel <rvanboxtel@roc-dev.com>
2018-04-18 12:39:36 +0200

    updates

---
+++ WebApi/Controllers/cursistsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class cursistsController : ApiController
+    {
+        private dewaaiEntities db = new dewaaiEntities();
+
+        // GET: api/cursists
+        public IQueryable<cursist> Getcursist()
+        {
+            return db.cursist;
+        }
+
+        // GET: api/cursists/5
+        [ResponseType(typeof(cursist))]
+        public IHttpActionResult Getcursist(string id)
+        {
+            cursist cursist = db.cursist.Find(id);
+            if (cursist == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cursist);
+        }
+
+        // PUT: api/cursists/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putcursist(string id, cursist cursist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cursist.EMAILADRES)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cursist).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!cursistExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/cursists
+        [ResponseType(typeof(cursist))]
+        public IHttpActionResult Postcursist(cursist cursist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.cursist.Add(cursist);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (cursistExists(cursist.EMAILADRES))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = cursist.EMAILADRES }, cursist);
+        }
+
+        // DELETE: api/cursists/5
+        [ResponseType(typeof(cursist))]
+        public IHttpActionResult Deletecursist(string id)
+        {
+            cursist cursist = db.cursist.Find(id);
+            if (cursist == null)
+            {
+                return NotFound();
+            }
+
+            db.cursist.Remove(cursist);
+            db.SaveChanges();
+
+            return Ok(cursist);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool cursistExists(string id)
+        {
+            return db.cursist.Count(e => e.EMAILADRES == id) > 0;
+        }
+    }
+}
---
+++ WebApi/Controllers/schipsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class schipsController : ApiController
+    {
+        private dewaaiEntities db = new dewaaiEntities();
+
+        // GET: api/schips
+        public IQueryable<schip> Getschip()
+        {
+            return db.schip;
+        }
+
+        // GET: api/schips/5
+        [ResponseType(typeof(schip))]
+        public IHttpActionResult Getschip(int id)
+        {
+            schip schip = db.schip.Find(id);
+            if (schip == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(schip);
+        }
+
+        // PUT: api/schips/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putschip(int id, schip schip)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != schip.NUMMER)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(schip).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!schipExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/schips
+        [ResponseType(typeof(schip))]
+        public IHttpActionResult Postschip(schip schip)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.schip.Add(schip);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (schipExists(schip.NUMMER))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = schip.NUMMER }, schip);
+        }
+
+        // DELETE: api/schips/5
+        [ResponseType(typeof(schip))]
+        public IHttpActionResult Deleteschip(int id)
+        {
+            schip schip = db.schip.Find(id);
+            if (schip == null)
+            {
+                return NotFound();
+            }
+
+            db.schip.Remove(schip);
+            db.SaveChanges();
+
+            return Ok(schip);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool schipExists(int id)
+        {
+            return db.schip.Count(e => e.NUMMER == id) > 0;
+        }
+    }
+}
